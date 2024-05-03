6ce53931fbdb05fd8e008b4db558fb9128b388d5
caobw046 <caobowen1105@163.com>
2024-05-03 14:37:34 +0800

    吸顶导航的交互

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+//引入初始化样式文件
+import '@/styles/common.scss'
+
+// 测试接口函数
+import { getCategoryAPI } from './apis/test'
+
+getCategoryAPI().then(res => {
+  console.log(res);
+})
 
 const app = createApp(App)
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from '@/views/Layout/components/LayoutFixed.vue'
+</script>
+
+<template>
+  // 吸顶导航栏组件
+  <LayoutFixed />
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
